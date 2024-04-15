447b7b6f150febf895a89580043f5a85a52a02dd
Alan <93069058+alanjuice@users.noreply.github.com>
2024-04-16 00:05:18 +0530

    add routes
    add route for geting all history
    add use-experiments(bug to fix)

--- app.js
+++ app.js
@@ -482,6 +482,17 @@ app.get("/getexperiment/:name", async (req, res) => {
   }
 });
 
+app.get("/gethistory", async (req, res) => {
+  try {
+    const histories = await ChemicalUsage.find();
+
+    res.json({ status: "ok", histories });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: "fail", message: "Internal server error" });
+  }
+});
+
 app.get("/reagents", async (req, res) => {
   try {
     // Query the database for all reagents and select only the reagentname field
@@ -615,6 +626,112 @@ app.post("/use-chemical", async (req, res) => {
   } catch (error) {
     console.error("Error using chemical:", error);
     res.status(500).json({ status: "fail", data: "Internal server error" });
+  }
+});
+
+app.use("/use-experiment", async (req, res) => {
+  try {
+    const { expname, batch, date, remark } = req.body;
+
+    // Find the experiment by name
+    const experiment = await Experiment.findOne({
+      name: expname,
+    });
+
+    if (!experiment) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Experiment not found" });
+    }
+
+    const chemicals = experiment.chemicalsUsed;
+    const reagents = experiment.reagentsUsed;
+
+    chemicals.forEach(async (chemical) => {
+      const quantity = chemical.quantity;
+      const chemicalname = chemical.chemicalName;
+      if (chemical.addquantity < quantity) {
+        return res.status(400).json({
+          status: "fail",
+          data: "Not enough quantity of the chemical available",
+        });
+      }
+
+      chemical.addquantity -= quantity;
+
+      const chemicalUsageSchema = new ChemicalUsage({
+        chemicalname,
+        quantity: quantity,
+        batch,
+        date,
+        remark,
+        usedAs: "Chemical",
+        name: "chemical",
+      });
+
+      await chemicalUsageSchema.save();
+      await chemical.save();
+    });
+
+    //For all the reagents in the experiment
+    reagents.forEach(async (reag) => {
+      const usedquantity = reag.quantity;
+      const reagentname = reag.reagentNameName;
+
+      const reagent = await Reagent.findOne({ reagentname });
+
+      // Check if the chemical exists
+      if (!reagent) {
+        return res
+          .status(404)
+          .json({ status: "fail", data: "Reagent not found" });
+      }
+
+      // Get all chemicals for that reagent
+      const chemicals = reagent.chemicals;
+
+      // Check if required quantity of chemicals is present
+      for (const chemical of chemicals) {
+        const chemicalname = chemical.chemicalname;
+        const chem = await Chemical.findOne({ chemicalname });
+
+        // Quantity of chemical needed = amount of reagent * chemical need for 1 reagent
+        const neededQuantity = usedquantity * chemical.quantity;
+
+        if (chem.addquantity < neededQuantity) {
+          return res.status(400).json({
+            status: "fail",
+            data: `${chemicalname} quantity not sufficient`,
+          });
+        }
+      }
+
+      // Deduct the required quantities of chemicals
+      for (const chemical of chemicals) {
+        const chemicalname = chemical.chemicalname;
+        const chem = await Chemical.findOne({ chemicalname });
+
+        // Quantity of chemical needed = amount of reagent * chemical need for 1 reagent
+        const neededQuantity = usedquantity * chemical.quantity;
+        chem.addquantity -= neededQuantity;
+
+        const chemicalUsageSchema = new ChemicalUsage({
+          chemicalname,
+          quantity: neededQuantity,
+          batch,
+          date,
+          remark,
+          usedAs: "Reagent",
+          name: reagentname,
+        });
+
+        await chemicalUsageSchema.save();
+        await chem.save();
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: "fail", message: "Internal server error" });
   }
 });
 
